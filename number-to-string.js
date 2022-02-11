e5864ff40b6d40c61b4f0a18430a63ad872468df
Will Frankland <will-frankland@hotmail.com>
2022-02-10 23:41:08 -0500

    Initial commit with my current work so far

---
+++ number-to-string.js
@@ -0,0 +1,10 @@
+const n = 128
+n.toString()
+
+console.log(n)
+
+
+
+Official Solution
+const n = 128
+   console.log(n.toString())
---
+++ rounding-numbers.js
@@ -0,0 +1,7 @@
+let roundUp = 1.5
+
+Math.round(roundUp, 0.5)
+
+let rounded = Math.round(roundUp)
+
+console.log(rounded)
